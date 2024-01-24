f6409ea1127d2cce4fc46eee4580de7a3e4ece58
Abraham D <abrahamobish@gmail.com>
2024-01-24 10:58:22 -0600

    Agregado constructor básico de matriz

--- Matriz.py
+++ Matriz.py
@@ -1,6 +1,28 @@
 class Matriz:
-    def __init__(self, data) -> None:
+    def __init__(self, *data) -> None:
+        """
+         0 1 2 3 4 5 6 7 8
+         | | | | 
+         V V V V
+        [1,2,3,4,5,6,7,8,9]
+        Explicar esto
+        """
+        self.data = []
+
+        n = int(len(data)**(1/2))
+
+        for i in range(n):
+            self.data.append([])
+            for j in range(n):
+                self.data[i].append(data[i*n+j])
+
+
+    def __init__(self, *data, n, m) -> None:
         pass
+
+        
+
+
 
     def __add__(self, other):
         pass
@@ -8,4 +30,6 @@ class Matriz:
     def __mul__(self, other: object):
         pass
 
-    
+
+if __name__ == "__main__":
+    pass
